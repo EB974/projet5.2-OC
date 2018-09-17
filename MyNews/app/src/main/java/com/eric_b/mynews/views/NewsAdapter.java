315304fb37293f15e777383d38aa49cfbedb821e
Eric_B <eric.bertaut@outlook.fr>
2018-09-17 11:56:17 +0400

    add news item layout

---
+++ MyNews/app/src/main/java/com/eric_b/mynews/views/NewsAdapter.java
@@ -0,0 +1,136 @@
+package com.eric_b.mynews.views;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
+import com.eric_b.mynews.R;
+import com.eric_b.mynews.models.Multimedium;
+import com.eric_b.mynews.models.Result;
+
+import java.util.List;
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
+
+    private List<Result> mItems;
+    private Context mContext;
+    private PostItemListener mItemListener;
+    private RequestManager glide;
+    private Multimedium multimedia;
+
+
+    public NewsAdapter(Context context,List<Result> results, RequestManager glide, PostItemListener itemListener) {
+        mContext = context;
+        mItemListener = itemListener;
+        mItems = results;
+        this.glide = glide;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        PostItemListener mItemListener;
+        ImageView imageTv;
+        TextView titleTv;
+        TextView dateTv;
+
+        public ViewHolder(View newsView, PostItemListener postItemListener) {
+            super(newsView);
+            titleTv = newsView.findViewById(R.id.news_item_title);
+            dateTv = newsView.findViewById(R.id.news_item_date);
+            imageTv = newsView.findViewById(R.id.news_item_image);
+            this.mItemListener = postItemListener;
+            newsView.setOnClickListener(this);
+            //CREATE VIEW HOLDER AND INFLATING ITS XML LAYOUT
+        }
+
+
+
+
+        @Override
+        public void onClick(View view) {
+            Result item = getResults(getAdapterPosition());
+            this.mItemListener.onPostClick(item.getShortUrl());
+            notifyDataSetChanged();
+        }
+    }
+
+
+    @Override
+    public NewsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View postView = inflater.inflate(R.layout.news_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(postView, this.mItemListener);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(NewsAdapter.ViewHolder holder, int position) {
+
+        Result item = mItems.get(position);
+        List<Multimedium> multimediaItems = item.getMultimedia();
+        try {
+            Multimedium multimedia = multimediaItems.get(0);
+            glide.load(multimedia.getUrl()).apply(RequestOptions.noTransformation()).into(holder.imageTv);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        TextView newsTitle;
+        TextView newsDate;
+
+        newsTitle = holder.titleTv;
+        newsDate = holder.dateTv;
+        newsTitle.setText(item.getTitle());
+        newsDate.setText(dateApater(item.getUpdatedDate()));
+        //multimedia = Multimedium;
+        //String photoUrl = multimedia.getUrl();
+        Log.d("repro","item "+multimediaItems.size());
+        //glide.load(multimedia.getUrl()).apply(RequestOptions.noTransformation()).into(holder.imageTv);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    public void updateAnswers(List<Result> items) {
+        mItems = items;
+        notifyDataSetChanged();
+    }
+
+    private Result getResults(int adapterPosition) {
+        return mItems.get(adapterPosition);
+    }
+
+    public interface PostItemListener {
+        void onPostClick(String title);
+    }
+
+    private String dateApater(String newsDate){
+        String mDate = newsDate;
+        String mDay = mDate.substring(0,10);
+
+        String format = "yyyy-MM-dd";
+        java.text.SimpleDateFormat formater = new java.text.SimpleDateFormat( format );
+        java.util.Date date = new java.util.Date();
+        String dayNewsDate = formater.format(date);
+
+        if(dayNewsDate.equals(mDay)){
+            mDay = "today";
+        };
+
+        newsDate = mDay+" "+mDate.substring(11,16);
+        return newsDate;
+    }
+
+
+}
